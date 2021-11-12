9f71260dca82a23976f7dacdc365ec4022bd64af
sreekrishnanparath <sreekrishnanvasudevapuri@gmail.com>
2021-11-12 21:40:36 +0000

    Initial commit

---
+++ src/main/java/com/springmaster/springbegin/SpringbeginApplication.java
@@ -0,0 +1,13 @@
+package com.springmaster.springbegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbeginApplication.class, args);
+	}
+
+}
